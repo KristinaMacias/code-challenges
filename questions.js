17572aad4ba908ae5f4a542b86e3bc899cea73f7
KristinaMacias <kristina@kristina.macias.com>
2022-07-29 14:03:26 -0700

    update questions and answers

--- questions.js
+++ questions.js
@@ -5,15 +5,17 @@
 
 /****/
 
+/****/
+
 //Use a function to multiply parameters a and b
 function multiply(a, b) {
-  return; //put code here
+  //write console log here
 }
 
 //Test Cases
-console.log(multiply(2, 3));
-console.log(multiply(5, 5));
-console.log(multiply(10, 10));
+multiply(2, 3); //6 passing in 2 and 3 as arguments to the function
+multiply(5, 5); //25 passing in 5 and 5 as arguments to the function
+multiply(10, 10); //100 passing 10 and 10 as arguments to the function
 
 /****/
 
