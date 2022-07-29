1301da5d19044d420a6a337264b972262abdfac2
KristinaMacias <kristina@kristina.macias.com>
2022-07-29 13:40:33 -0700

    questions and solutions

---
+++ questions.js
@@ -0,0 +1,20 @@
+//Code Wars & Other Code Challenges
+//This scripts sheet contains the questions only
+
+//Easy
+
+/****/
+
+//Use a function to multiply parameters a and b
+function multiply(a, b) {
+  return; //put code here
+}
+
+//Test Cases
+console.log(multiply(2, 3));
+console.log(multiply(5, 5));
+console.log(multiply(10, 10));
+
+/****/
+
+//Intermediate
---
+++ solutions.js
@@ -0,0 +1,20 @@
+//Code Wars
+//This scripts sheet contains the solutions
+
+//Easy
+
+/****/
+
+//Use a function to multiply parameters a and b
+console.log("Use a function to multiply parameters a and b:");
+function multiply(a, b) {
+  //a and b are parameters
+  console.log(a * b); //printing a * b
+}
+
+//Test Cases
+multiply(2, 3); //6 passing in 2 and 3 as arguments to the function
+multiply(5, 5); //25 passing in 5 and 5 as arguments to the function
+multiply(10, 10); //100 passing 10 and 10 as arguments to the function
+
+/****/
